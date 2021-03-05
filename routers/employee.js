eb49beffccd8326152e1bc5f2ff4d4091534faa0
shivam kosta <corporate19shivamkosta@gmail.com>
2021-03-05 16:28:17 +0530

    finally

--- routers/employee.js
+++ routers/employee.js
@@ -29,13 +29,12 @@ router.get('/get/read/:id',(req,res)=>{
     })
 })
 
-//CREATE API
-router.post('/submit',(req,res,next)=>{
+//Create Employee details
+router.post('/empdetails',(req,res,next)=>{
     console.log(req.body);
     connection.query(
-        "insert into empdetail(fname,lname,dob,email,gender,matrimony,mobile,doj,presentaddress,permanentaddress,bankname,ifsccode,bankaccount) value(?,?,?,?,?,?,?,?,?,?,?,?,?)",
-        [
-            
+        "insert into empdetail(fname,lname,dob,email,gender,matrimony,mobile,doj,presentaddress,permanentaddress,bankname,ifsccode,bankaccount) values(?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        [            
             req.body.fname,
             req.body.lname,
             req.body.dob,
@@ -48,13 +47,7 @@ router.post('/submit',(req,res,next)=>{
             req.body.permanentaddress,
             req.body.bankname,
             req.body.ifsccode,
-            req.body.bankaccount,
-            // req.files[0].originalname,
-            // req.files[1].originalname,
-            // req.files[2].originalname,
-            // req.files[3].originalname,
-            // req.files[4].originalname,
-            
+            req.body.bankaccount,          
 
         ],
         (err,result)=>{
@@ -70,19 +63,104 @@ router.post('/submit',(req,res,next)=>{
     )
 });
 
-//UPDATE API
-router.put('/update/employee/:id',(req,res,next)=>{
+// //Create Bank Account details
+// router.post('/bankdetails/:id',(req,res,next)=>{
+//     console.log('bankaccount api is running')
+//     console.log(req.params.id);
+//     //sql query
+//     let sql = `INSERT INTO empdetail(bankname,ifsccode,bankaccount)
+//                VALUES('${req.body.bankname}','${req.body.ifsccode}','${req.body.bankaccount}')
+//                WHERE id = '${req.params.id}'`;
+    
+//     //run query
+//     connection.query(sql,(err,result)=>{
+//         if(err) throw err;
+//         res.status(200).json({SUCCESS:true})
+//         console.log(result);  
+//     })
+// })
+
+// // Create Emergency Contact Details
+// router.post('/emergencydetails',(req,res,next)=>{
+//     console.log(req.params.id);
+    
+//     //sql query
+//     let sql = `INSERT INTO empdetail(fname,relation,mobile,address),
+//                VALUES('${req.body.fname}','${req.body.relation}','${req.body.mobile}','${req.body.presentaddress}')
+//                WHERE id = '${req.params.id}'`;
+    
+//     //run query
+//     connection.query(sql,(err,result)=>{
+//         if(err) throw err;
+//         res.status(200).json({SUCCESS:true})
+//         console.log(result);  
+//     })
+
+        
+// });
+
+
+//UPDATE Employee Details
+router.put('/update/empdetails/:id',(req,res,next)=>{
+    console.log(req.params.id);
+
+    var x = req.body;
+    var fname = x.fname;
+    var lname = x.lname;
+    var dob = x.dob;
+    var email = x.email;
+    var gender = x.gender;
+    var matrimony = x.matrimony;
+    var mobile = x.mobile;
+    var doj = x.doj;
+    var presentaddress = x.presentaddress;
+    var permanentaddress = x.permanentaddress;
+
+    connection.query(`Update empdetail SET fname=?,lname=?,dob=?,email=?,gender=?,matrimony=?,mobile=?,doj=?,presentaddress=?,permanentaddress=? WHERE id='${req.params.id}'`
+    ,[fname,lname,dob,email,gender,matrimony,mobile,doj,presentaddress,permanentaddress],
+    function(err,result){
+        if(err) throw err;
+        res.json({SUCCESS:true,message:'User has been updated successfully'})
+        console.log(result)
+    })
+
+});
+
+//Update bank details
+router.put('/update/bankdetails/:id',(req,res,next)=>{
     console.log(req.params.id);
     
-    //sql query
-    let sql = `UPDATE empdetail SET  
-               fname = '${req.body.fname}'               
-               WHERE id = '${req.params.id}'`;
+    var x = req.body;
+    var bankname = x.bankname;
+    var ifsccode = x.ifsccode;
+    var bankaccount = x.bankaccount;
+    
 
-    //run query
-    connection.query(sql,(err,result)=>{
+    connection.query(`Update empdetail SET bankname=?,ifsccode=?,bankaccount=? WHERE id='${req.params.id}'`
+    ,[bankname,ifsccode,bankaccount],
+    function(err,result){
         if(err) throw err;
-        res.json({SUCCESS:true,message:'data updated'});
+        res.json({SUCCESS:true,message:'User has been updated successfully'})
+        console.log(result)
+    })
+});
+
+//update emergency details
+router.put('/update/emergencydetails/:id',(req,res,next)=>{
+    console.log(req.params.id);
+    
+    var x = req.body;
+    var fname = x.fname;
+    var relation = x.relation;
+    var mobile = x.mobile;
+    var presentaddress = x.presentaddress;
+    
+
+    connection.query(`Update empdetail SET fname=?,relation=?,mobile=?,presentaddress=? WHERE id='${req.params.id}'`
+    ,[fname,relation,mobile,presentaddress],
+    function(err,result){
+        if(err) throw err;
+        res.json({SUCCESS:true,message:'User has been updated successfully'})
         console.log(result)
     })
 })
